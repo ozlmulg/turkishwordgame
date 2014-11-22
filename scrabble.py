282f0e801807f8fc797e53ef515968b8be4ff824
ozlmulg <ozlmulg@hotmail.com>
2014-11-22 02:47:58 +0200

    Swap Target with last element

--- scrabble.py
+++ scrabble.py
@@ -15,12 +15,16 @@ def main():
     MouseReleased=False # Released THIS FRAME
     Target=None # target of Drag/Drop    
 # ################################################################
-
+    global tilem
+    tilem = None
+# #####################
+    #playedTiles = []
+# #####################
     while True: #show SCREEN until not close
 	#SCREEN.fill((0,0,0)) # clear screen
 	SCREEN.blit(asurf, imagerect)
-	
 	pos=pygame.mouse.get_pos()
+
 	
 	for event in pygame.event.get(): #get event from pygame
 	    if event.type == pygame.QUIT: #if user click close button
@@ -50,8 +54,22 @@ def main():
 		        pos[1] >= (item.coordinate[1]) and # ######## Changed
 		        pos[1] <= (item.coordinate[1] + item.SQUARE_SIZE) ): # inside the bounding box
 			Target=item # "pick up" item
+			# ##############################################SWAP##############
+			indexofTarget = players[active].tray.index(Target)
+			temp = players[active].tray[-1] #last element of tray
+			players[active].tray[indexofTarget] = temp
+			players[active].tray[-1] = Target# now last element is Target
+			# ####################################################
 			break
-		
+	        '''
+		for item in playedTiles: # search all items
+		    if (pos[0] >= (item.coordinate[0]) and # ######## Changed
+		        pos[0] <= (item.coordinate[0] + item.SQUARE_SIZE) and 
+		        pos[1] >= (item.coordinate[1]) and # ######## Changed
+		        pos[1] <= (item.coordinate[1] + item.SQUARE_SIZE) ): # inside the bounding box
+			Target=item # "pick up" item
+			break		
+		'''
 		
 		#if Target is None: # didn't find any?
 		    #Target=Disk((0,0,255),coordinate,32) # create a new one
@@ -60,7 +78,7 @@ def main():
 	    if MouseDown and Target is not None: # if we are dragging something
 		Target.coordinate = (pos[0] - tile.Tile.SQUARE_SIZE/2, pos[1] - tile.Tile.SQUARE_SIZE/2) # move the target with us ############Holds the tile from the middle
 	    
-	    if MouseReleased:
+	    if MouseReleased and Target is not None:
 		# ##############################
 		if (Target.coordinate[0]%36-5) <= tile.Tile.SQUARE_SIZE/2 and (Target.coordinate[1]%36-5) <= tile.Tile.SQUARE_SIZE/2:
 		    Target.coordinate = ((Target.coordinate[0] - (Target.coordinate[0] % 36)) + 5, (Target.coordinate[1] - (Target.coordinate[1] % 36)) + 5)
@@ -71,9 +89,18 @@ def main():
 		elif(Target.coordinate[0]%36-5) > tile.Tile.SQUARE_SIZE/2 and (Target.coordinate[1]%36-5) > tile.Tile.SQUARE_SIZE/2:
 		    Target.coordinate = ((Target.coordinate[0] + (36-Target.coordinate[0]%36)) + 5, (Target.coordinate[1] + (36-Target.coordinate[1]%36)) + 5)
 		# ##############################
-				    
-		Target = None # Drop item, if we have any
+		# ############
+		'''
+		if(Target in players[active].tray):
+		    players[active].tray.remove(Target)
+		    playedTiles.append(Target)
+	        '''
+
 		
+		# ############
+		#Target.draw(False)
+		#tilem = Target
+		Target = None # Drop item, if we have any
 		
 		
 	    MousePressed = False # Reset these to False
@@ -81,7 +108,11 @@ def main():
 
 # ###############
 	#gameboard.draw()
-	players[active].drawTray(SCREEN)#draw tiles of the player ## SILINECEK NOT: BU KOD SATIRININ YERI ONEMLI   
+	players[active].drawTray(SCREEN)#draw tiles of the player ## SILINECEK NOT: BU KOD SATIRININ YERI ONEMLI 
+	'''
+	for item in playedTiles:
+	    item.draw(False)
+	'''
 # #############
         pygame.display.update()    
 
