c222b55d2bdc93e58d511ef360dc4c5270d02a35
ozlmulg <ozlmulg@hotmail.com>
2014-11-30 14:52:10 +0200

    tile placement

--- scrabble.py
+++ scrabble.py
@@ -1,6 +1,6 @@
 import pygame,sys
 from pygame.locals import *
-import board,bag,menu,human,tile
+import board,bag,menu,human,tile,player
 
 
 #Run the game
@@ -15,17 +15,26 @@ def main():
     MouseReleased=False # Released THIS FRAME
     Target=None # target of Drag/Drop    
 # ################################################################
-    #global tilem
-    #tilem = None
-# #####################
-    playedTiles = []
-# #####################
+    global tilem
+    tilem = None
+# ##################### All positions that any tile can be at and their status of occupy, both board and tray 
+    
+    squares = {}
+    
+    for i in range (board.Board.GRID_SIZE):
+	for j in range (board.Board.GRID_SIZE):
+	    squares[(5 + j * (board.Board.SQUARE_SIZE + board.Board.SQUARE_BORDER), 5 + i * (board.Board.SQUARE_SIZE + board.Board.SQUARE_BORDER))] = 0
+	    
+	    
+    for i in range (player.Player.TRAY_SIZE):
+	    squares[human.Human.TRAY_FIRSTLEFT + i * (board.Board.SQUARE_SIZE + board.Board.SQUARE_BORDER), human.Human.TRAY_FIRSTTOP] = 1
+    
+	                 
+    
+# ##################### All positions that any tile can be at and their status of occupy, both board and tray 
+
+
     Target=None # target of Drag/Drop 
-    # ##############save initial image
-    global asurf
-    asurf = pygame.image.load('Board.jpg')   
-    # ##############
-    # ###############
     while True: #show SCREEN until not close
 	
 	#SCREEN.blit(asurf, imagerect)
@@ -46,16 +55,6 @@ def main():
 	    if event.type == MOUSEBUTTONDOWN:
 		if menu.playButton.pressed(pygame.mouse.get_pos()):
 		    print "Play pressed!"
-		    # #################save image when it placed#########
-		    players[active].drawTray(SCREEN)
-		    for Target in playedTiles:
-			players[active].tray.remove(Target)		    
-		    playedTiles = []
-		    rect = pygame.Rect(0, 0, 545, 545)
-		    sub = SCREEN.subsurface(rect)
-		    pygame.image.save(sub, "Board.png")
-		    asurf = pygame.image.load('Board.png')
-		    #  ##############################################	    
 		if menu.changeButton.pressed(pygame.mouse.get_pos()):
 		    print "Shuffle pressed!"
 		if menu.backButton.pressed(pygame.mouse.get_pos()):
@@ -76,6 +75,9 @@ def main():
 		        pos[1] >= (item.coordinate[1]) and # ######## Changed
 		        pos[1] <= (item.coordinate[1] + item.SQUARE_SIZE) ): # inside the bounding box
 			Target=item # "pick up" item
+			# ####   NEW
+			squares[Target.coordinate] = 0 # Update the position as empty
+			# ####   NEW
 			Target.oldPos = Target.coordinate[:]
 			# ##############################################SWAP##############
 			indexofTarget = players[active].tray.index(Target)
@@ -100,49 +102,101 @@ def main():
 		
 	    if MouseDown and Target is not None: # if we are dragging something
 		Target.coordinate = (pos[0] - tile.Tile.SQUARE_SIZE/2, pos[1] - tile.Tile.SQUARE_SIZE/2) # move the target with us ############Holds the tile from the middle
+		
+		slideTile = 4 # if a tile is dropped on another tile, slide the tile to the position which was closer
 	    
 	    if MouseReleased and Target is not None:
-		# ##############################
-		if Target.coordinate[0] >= board.Board.GRID_SIZE * (board.Board.SQUARE_SIZE + board.Board.SQUARE_BORDER) + 1 or Target.coordinate[1] >= board.Board.GRID_SIZE * (board.Board.SQUARE_SIZE + board.Board.SQUARE_BORDER) + 1:
-				    Target.coordinate = Target.oldPos		
-		elif (Target.coordinate[0]%36-5) <= tile.Tile.SQUARE_SIZE/2 and (Target.coordinate[1]%36-5) <= tile.Tile.SQUARE_SIZE/2:
-		    Target.coordinate = ((Target.coordinate[0] - (Target.coordinate[0] % 36)) + 5, (Target.coordinate[1] - (Target.coordinate[1] % 36)) + 5)
-		    # ###############will remove from tray when played#############3
-		    if(Target not in playedTiles):
-			playedTiles.append(Target)
-		    # ######################################################
-		elif(Target.coordinate[0]%36-5) > tile.Tile.SQUARE_SIZE/2 and (Target.coordinate[1]%36-5) <= tile.Tile.SQUARE_SIZE/2:
-		    Target.coordinate = ((Target.coordinate[0] + (36-Target.coordinate[0]%36)) + 5, (Target.coordinate[1] - (Target.coordinate[1] % 36)) + 5)
-		    if(Target not in playedTiles):
-		        playedTiles.append(Target)
-		elif(Target.coordinate[0]%36-5) <= tile.Tile.SQUARE_SIZE/2 and (Target.coordinate[1]%36-5) > tile.Tile.SQUARE_SIZE/2:
-		    Target.coordinate = ((Target.coordinate[0] - (Target.coordinate[0] % 36)) + 5, (Target.coordinate[1] + (36-Target.coordinate[1]%36)) + 5)
-		    if(Target not in playedTiles):
-		        playedTiles.append(Target)
-		elif(Target.coordinate[0]%36-5) > tile.Tile.SQUARE_SIZE/2 and (Target.coordinate[1]%36-5) > tile.Tile.SQUARE_SIZE/2:
-		    Target.coordinate = ((Target.coordinate[0] + (36-Target.coordinate[0]%36)) + 5, (Target.coordinate[1] + (36-Target.coordinate[1]%36)) + 5)
-		    if(Target not in playedTiles):
-		        playedTiles.append(Target)
-		# ##############################
-		# ############
+		#  fit the tile in the positions
+		if (Target.coordinate[0] < board.Board.GRID_SIZE * (board.Board.SQUARE_SIZE + board.Board.SQUARE_BORDER) - board.Board.SQUARE_SIZE/2 + 1 and Target.coordinate[1] < board.Board.GRID_SIZE * (board.Board.SQUARE_SIZE + board.Board.SQUARE_BORDER) - board.Board.SQUARE_SIZE/2 + 1 and Target.coordinate[0] > board.Board.SQUARE_BORDER + 1 - board.Board.SQUARE_SIZE/2 and Target.coordinate[1] > board.Board.SQUARE_BORDER + 1 - board.Board.SQUARE_SIZE/2) :
+		    i = 5
+		    a = Target.coordinate
+		    while i <= Target.coordinate[0] + 16 :
+			i = i + 36
+			
+		    Target.coordinate = (i - 36, Target.coordinate[1])
+		    
+		    i = 5
+		    while i <= Target.coordinate[1] + 16 :
+			i = i + 36
+		    
+		    Target.coordinate = (Target.coordinate[0], i - 36)	
+		    
+		# if the tile put on tray
+		elif (Target.coordinate[0] > human.Human.TRAY_LEFT and Target.coordinate[1] > human.Human.TRAY_TOP and Target.coordinate[0] < human.Human.TRAY_LEFT + (tile.Tile.SQUARE_SIZE + tile.Tile.SQUARE_BORDER)*8 and Target.coordinate[1] < human.Human.TRAY_TOP + tile.Tile.SQUARE_SIZE + tile.Tile.SQUARE_BORDER*2):
+		    i = human.Human.TRAY_FIRSTLEFT
+		    a = Target.coordinate
+		    while i <= Target.coordinate[0] + 16 :
+			i = i + 36		    
+			
+		    Target.coordinate = (i - 36, human.Human.TRAY_FIRSTTOP)
+		    
+		    
+		    
+		else:
+		    Target.coordinate = Target.oldPos # if tile put out of board bring it back
+		# fit the tile in the positions - end
 		
-		#if(Target in players[active].tray):
-		    #players[active].tray.remove(Target)
-		    #playedTiles.append(Target)
+		
+		# slide the tile if it is dropped on another tile
+		#for t in players[active].tray[:player.Player.TRAY_SIZE-1]: # search all the tray but not last element which is the tile itself
+		    #if t.coordinate == Target.coordinate:
+		slidingTo = 4
+		if squares[Target.coordinate] == 1:
+		    if Target.coordinate[0] <= a[0] and Target.coordinate[1] <= a[1]:
+			if a[0]-Target.coordinate[0]>=a[1]-Target.coordinate[1]:
+			    Target.coordinate = (Target.coordinate[0] + 36, Target.coordinate[1])
+			    slidingTo = 1
+			else:
+			    Target.coordinate = (Target.coordinate[0], Target.coordinate[1] + 36)
+			    slidingTo = 3
+			    
+		    elif Target.coordinate[0] > a[0] and Target.coordinate[1] <= a[1]:
+			if Target.coordinate[0] - a[0] >= a[1] - Target.coordinate[1]:
+			    Target.coordinate = (Target.coordinate[0] - 36, Target.coordinate[1])
+			    slidingTo = 0
+			else:
+			    Target.coordinate = (Target.coordinate[0], Target.coordinate[1] + 36)
+			    slidingTo = 3
+			    
+		    elif Target.coordinate[0] > a[0] and Target.coordinate[1] > a[1]:
+			if Target.coordinate[0] - a[0] >= Target.coordinate[1] - a[1]:
+			    Target.coordinate = (Target.coordinate[0] - 36, Target.coordinate[1])
+			    slidingTo = 0
+			else:
+			    Target.coordinate = (Target.coordinate[0], Target.coordinate[1] - 36)
+			    slidingTo = 2
+			    
+		    elif Target.coordinate[0] <= a[0] and Target.coordinate[1] > a[1]:
+			if a[0]-Target.coordinate[0] >= Target.coordinate[1] - a[1]:
+			    Target.coordinate = (Target.coordinate[0] + 36, Target.coordinate[1])
+			    slidingTo = 1
+			else:
+			    Target.coordinate = (Target.coordinate[0], Target.coordinate[1] - 36)
+			    slidingTo = 2
+			    
+		    while squares[Target.coordinate] == 1:
+			if slidingTo == 0:
+			    Target.coordinate = (Target.coordinate[0] + 36, Target.coordinate[1])
+			elif slidingTo == 1:
+			    Target.coordinate = (Target.coordinate[0] - 36, Target.coordinate[1])
+			elif slidingTo == 2:
+			    Target.coordinate = (Target.coordinate[0], Target.coordinate[1] + 36)
+			elif slidingTo == 3:
+			    Target.coordinate = (Target.coordinate[0], Target.coordinate[1] - 36)
+			    
+			
+	    # slide the tile if it is dropped on another tile
+			    
+		squares[Target.coordinate] = 1
 
-		# ############
-		#Target.draw(False)
-		#tilem = Target
 		Target = None # Drop item, if we have any
-		
-		
+				
 	    MousePressed = False # Reset these to False
 	    MouseReleased = False # Ditto
 
 # ###############
 	#gameboard.draw()
 	players[active].drawTray(SCREEN)#draw tiles of the player ## SILINECEK NOT: BU KOD SATIRININ YERI ONEMLI 
-	
 	'''
 	for item in playedTiles:
 	    item.draw(False)
@@ -161,9 +215,9 @@ def draw():
 
     global gamemenu
     gamemenu = menu.GameMenu()#create game menu
-    #global asurf
-    #global imagerect
-    #asurf = pygame.image.load('Board.jpg')
+    global asurf
+    global imagerect
+    asurf = pygame.image.load('Board.jpg')
     #imagerect = asurf.get_rect()
     SCREEN.fill((0,0,0))
     #SCREEN.blit(asurf.convert_alpha(),[0,0])
